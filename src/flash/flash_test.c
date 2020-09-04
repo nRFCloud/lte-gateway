d9bb7a3fe0adc7d1a94091f71b907a475b9a1ad8
Pete Skeggs <peter.skeggs@nordicsemi.no>
2020-09-04 15:37:50 -0700

    Add flash chip verification test module

---
+++ src/flash/flash_test.c
@@ -0,0 +1,146 @@
+#include <zephyr.h>
+#include <drivers/flash.h>
+#include <drivers/gpio.h>
+#include <stdio.h>
+#include <string.h>
+
+#if (CONFIG_SPI_FLASH_W25QXXDV - 0)
+/* NB: W25Q16DV is a JEDEC spi-nor device, but has a separate driver. */
+#define FLASH_DEVICE CONFIG_SPI_FLASH_W25QXXDV_DRV_NAME
+#define FLASH_NAME "W25QXXDV"
+#elif (CONFIG_SPI_NOR - 0) || \
+    DT_NODE_HAS_STATUS(DT_INST(0, jedec_spi_nor), okay)
+#define FLASH_DEVICE DT_LABEL(DT_INST(0, jedec_spi_nor))
+#define FLASH_NAME "JEDEC SPI-NOR"
+#elif (CONFIG_NORDIC_QSPI_NOR - 0) || \
+    DT_NODE_HAS_STATUS(DT_INST(0, nordic_qspi_nor), okay)
+#define FLASH_DEVICE DT_LABEL(DT_INST(0, nordic_qspi_nor))
+#define FLASH_NAME "JEDEC QSPI-NOR"
+#else
+#error Unsupported flash driver
+#endif
+
+#if defined(CONFIG_BOARD_ADAFRUIT_FEATHER_STM32F405)
+#define FLASH_TEST_REGION_OFFSET 0xf000
+#else
+#define FLASH_TEST_REGION_OFFSET 0xff000
+#endif
+#define FLASH_SECTOR_SIZE 4096
+
+#define EXT_MEM_CTRL_PIN 9
+
+/**@brief Set the external mem control pin to high to 
+  * enable access to the external memory chip. */
+static uint8_t flash_test_init()
+{
+    struct device *port;
+    int err;
+
+    port = device_get_binding(DT_LABEL(DT_NODELABEL(gpio0)));
+    if (!port)
+    {
+        return -EIO;
+    }
+
+    err = gpio_pin_configure(port, EXT_MEM_CTRL_PIN, GPIO_OUTPUT_HIGH);
+    if (err)
+    {
+        return err;
+    }
+
+    err = gpio_pin_set(port, EXT_MEM_CTRL_PIN, 1);
+    if (err)
+    {
+        return err;
+    }
+
+    return err;
+}
+SYS_INIT(flash_test_init, POST_KERNEL, CONFIG_SPI_NOR_INIT_PRIORITY);
+
+static void flash_test()
+{
+    const u8_t expected[] = {0x55, 0xaa, 0x66, 0x99};
+    const size_t len = sizeof(expected);
+    u8_t buf[sizeof(expected)];
+    struct device *flash_dev;
+    int rc;
+
+    printf("\n" FLASH_NAME " SPI flash testing\n");
+    printf("==========================\n");
+
+    if(!flash_test_init())
+    {
+        printf("SPI Control init failed\n");
+    }
+
+    flash_dev = device_get_binding(FLASH_DEVICE);
+
+    if (!flash_dev)
+    {
+        printf("SPI flash driver %s was not found!\n",
+            FLASH_DEVICE);
+        return;
+    }
+
+    /* Write protection needs to be disabled before each write or
+        * erase, since the flash component turns on write protection
+        * automatically after completion of write and erase
+        * operations.
+        */
+    printf("\nTest 1: Flash erase\n");
+    flash_write_protection_set(flash_dev, false);
+
+    rc = flash_erase(flash_dev, FLASH_TEST_REGION_OFFSET,
+                    FLASH_SECTOR_SIZE);
+    if (rc != 0)
+    {
+        printf("Flash erase failed! %d\n", rc);
+    }
+    else
+    {
+        printf("Flash erase succeeded!\n");
+    }
+
+    printf("\nTest 2: Flash write\n");
+    flash_write_protection_set(flash_dev, false);
+
+    printf("Attempting to write %u bytes\n", len);
+    rc = flash_write(flash_dev, FLASH_TEST_REGION_OFFSET, expected, len);
+    if (rc != 0)
+    {
+        printf("Flash write failed! %d\n", rc);
+        return;
+    }
+
+    memset(buf, 0, len);
+    rc = flash_read(flash_dev, FLASH_TEST_REGION_OFFSET, buf, len);
+    if (rc != 0)
+    {
+        printf("Flash read failed! %d\n", rc);
+        return;
+    }
+
+    if (memcmp(expected, buf, len) == 0)
+    {
+        printf("Data read matches data written. Good!!\n");
+    }
+    else
+    {
+        const u8_t *wp = expected;
+        const u8_t *rp = buf;
+        const u8_t *rpe = rp + len;
+
+        printf("Data read does not match data written!!\n");
+        while (rp < rpe)
+        {
+            printf("%08x wrote %02x read %02x %s\n",
+                (u32_t)(FLASH_TEST_REGION_OFFSET + (rp - buf)),
+                *wp, *rp, (*rp == *wp) ? "match" : "MISMATCH");
+            ++rp;
+            ++wp;
+        }
+    }
+
+}
+SYS_INIT(flash_test, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
